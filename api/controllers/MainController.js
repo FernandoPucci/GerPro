524e1425d401d637336e86c4cba5ea1116d2afef
FernandoPucci <fsilvapucci@gmail.com>
2017-05-05 00:12:26 -0300

    Add CronServices

--- api/controllers/MainController.js
+++ api/controllers/MainController.js
@@ -4,8 +4,50 @@
  * @description :: Server-side logic for managing mains
  * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
  */
+var bodyParser = require("body-parser");
 
 module.exports = {
-	
+
+    start: function (req, res) {
+
+        var startResponse = CronServices.startCron();
+
+        if (typeof startResponse === 'number') {
+
+            res.status(startResponse).end(startResponse + "");
+
+        } else {
+
+            res.status(500).end(startResponse + "");
+
+        }
+
+    },
+
+    stop: function (req, res) {
+
+        var stopResponse = CronServices.stopCron();
+
+        if (typeof stopResponse === 'number') {
+
+            res.status(stopResponse).end(stopResponse + "");
+
+        } else {
+
+            res.status(500).end(stopResponse + "");
+        }
+
+    },
+
+    healthcheck: function (req, res) {
+
+        var checkResponse = CronServices.checkCron();
+
+
+            res.status(200).end(checkResponse + "");
+       
+
+    }
+
 };
 
---
+++ api/services/CronServices.js
@@ -0,0 +1,112 @@
+var cron = require('node-cron');
+
+////////// UTILS
+var logFunction = function log() {
+    return "Function fired at: >>> " + new Date().toLocaleString();
+};
+
+var messageTestFunction = function (req, res) {
+
+    MessageServices.sendMessage("fsilvapucci@gmail.com",
+        "Cron Message = " + new Date().toLocaleString(),
+        "This is an automatically Cron Message = " + new Date().toLocaleString(),
+        res);
+};
+
+var lastExecution = "N/A";
+
+var task = null;
+
+//USAGE ONLY IN DEVELOPMENT
+//FOR FUTURE SPRINTS, EXPRESSION WILL SEND IN POST, ON START
+
+/// Node-Cron Instructions
+/**
+ * 
+ # ┌────────────── second (optional)
+ # │ ┌──────────── minute
+ # │ │ ┌────────── hour
+ # │ │ │ ┌──────── day of month
+ # │ │ │ │ ┌────── month
+ # │ │ │ │ │ ┌──── day of week
+ # │ │ │ │ │ │
+ # │ │ │ │ │ │
+ # * * * * * *
+
+ Example:
+
+ Every second divisible by 10 (ended with 0)
+ "* /10 * * * * *"
+ */
+var cronExpression = '0 */5 * * * *';//5 minutes
+
+var CronServices = {
+
+    startCron: function fcnStartCron() {
+
+        sails.log("Starting CRON feature");
+
+        try {
+            task = cron.schedule(cronExpression, function () {
+                console.log(logFunction());
+                messageTestFunction();
+                lastExecution = new Date().toLocaleString();
+
+            });
+
+            task.start();
+
+        } catch (err) {
+            sails.log.error("Function START ERROR: " + err);
+            return ("Function START ERROR: " + err);
+        }
+
+        sails.log("DONE");
+        return 200;
+
+    },
+
+    stopCron: function fcnStopCron() {
+
+        sails.log("Stopping CRON feature");
+
+        try {
+            if (task !== null) {
+
+                task.stop();
+                task = null;
+
+            } else {
+                sails.log.error("There's NOT any TASK running!");
+                return ("There's NOT any TASK running!");
+            }
+
+        } catch (err) {
+            sails.log.error("Function STOP ERROR: " + err);
+            return ("Function STOP ERROR: " + err);
+        }
+
+        sails.log("DONE");
+        return 200;
+    },
+
+    checkCron: function fcnCheckCron() {
+
+        sails.log("Checking CRON status");
+
+        if (task !== null) {
+            sails.log("HEALTHCHECK:\nTASK running!\n" +
+                "Configuration: " + cronExpression +
+                "\nLast Execution at: " + lastExecution);
+
+            return ("HEALTHCHECK:\nTASK running!\n" +
+                "Configuration: " + cronExpression +
+                "\nLast Execution at: " + lastExecution);
+        } else {
+            sails.log("HEALTHCHECK:\nThere's NOT any TASK running!");
+            return ("HEALTHCHECK:\nThere's NOT any TASK running!");
+        }
+    }
+};
+
+module.exports = CronServices; 
