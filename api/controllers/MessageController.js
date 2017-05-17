728adfbef20c2dde4c3e0176de24fd99f43e7b73
FernandoPucci <fsilvapucci@gmail.com>
2017-05-16 21:10:18 -0300

    Call Procedure/Function Controller Method

--- api/controllers/MessageController.js
+++ api/controllers/MessageController.js
@@ -7,17 +7,31 @@
 var bodyParser = require("body-parser");
 
 module.exports = {
+    //Disabling REST for this Controller
+    // _config: {
+    //     actions: false,
+    //     shortcuts: false,
+    //     rest: false
+    // },
 
     //use TestService
     sendMessage: function (req, res) {
 
         var testMsg = MessageServices.sendMessage(req.body.to,
-                                                req.body.subject,
-                                                req.body.message,
-                                                res);
+            req.body.subject,
+            req.body.message,
+            res);
 
         res.status(200).end("OK " + testMsg);
 
+    },
+    callMessages: function (req, res) {
+        
+        Message.query("SELECT ALL_MESSAGES_PRC()", function (err, results) {
+            if (err)
+                return res.serverError(err);
+            return res.ok(results.rows);
+        });
     }
 };
 
