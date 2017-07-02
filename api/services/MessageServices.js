2e73d69e74e9afe7aafbe6da3ff30f463f4586d6
Fernando Silva Pucci <fsilvapucci@gmail.com>
2017-07-02 20:01:01 -0300

    Disable Keys
    Do not merge this keys

--- api/services/MessageServices.js
+++ api/services/MessageServices.js
@@ -10,8 +10,8 @@ var bodyParser = require("body-parser");
 //#########     MAILGUN    ###############
 //########################################
 
-var api_key = 'key-8b0b023f19ab06f278dc2db893097b96';
-var domain = 'sandbox144485e953204a3aae1777988fc008be.mailgun.org';
+var api_key = 'key-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx';
+var domain = 'sandboxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx.mailgun.org';
 var mailgun = require('mailgun-js')({ apiKey: api_key, domain: domain });
 
 var _from = 'GERPRO no-reply <mailgun@sandbox144485e953204a3aae1777988fc008be.mailgun.org>';
