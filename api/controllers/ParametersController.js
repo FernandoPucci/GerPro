8576cce9fb2ffd4986a9426f4f5ba37d348a1ff1
FernandoPucci <fsilvapucci@gmail.com>
2017-09-02 14:01:41 -0300

    Update Parameters Controller

--- api/controllers/ParametersController.js
+++ api/controllers/ParametersController.js
@@ -3,6 +3,14 @@
  *
  * @description :: Server-side logic for managing Parameters
  * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ * 
+ * To Select Parameters by CATEGORY_NAME: 
+ * /api/parameters?category_name=<cat_name>
+ * 
+ * To Get all available categories:
+ * /api/parameters/categories
+ * 
+ * 
  */
 
 module.exports = {
