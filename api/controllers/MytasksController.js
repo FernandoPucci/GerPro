589c8b6232b2baa6b8eb0fcaae413052a5d0336b
Luis Fernando <luisfoxi@hotmail.com>
2018-03-07 09:40:36 -0300

    Implementação para api/mytasks receber o parâmetro user_checker_id=id_do_usuário

--- api/controllers/MytasksController.js
+++ api/controllers/MytasksController.js
@@ -7,7 +7,9 @@
 
 module.exports = {
 	index: function (req, res) {
-        TaskChecks.query("SELECT json_array_elements(next_executions_as_json(2)) AS task_check;", function (err, results) {
+        user_checker_id = req.param('user_checker_id');
+        if(user_checker_id == undefined) user_checker_id = null;
+        TaskChecks.query("SELECT json_array_elements(next_executions_as_json(" + user_checker_id + ")) AS task_check;", function (err, results) {
             if (err)
                 return res.serverError(err);
             return res.ok(results.rows);
